9186691e526957d684bee03861c363fcb21a35f7
Luke Graham <luke.evan.graham@gmail.com>
2026-06-09 22:07:19 -0700

    full width gallery

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -3,6 +3,11 @@ module.exports = {
     return a === b;
   },
   getCloudinaryResize: (url, width) => {
-    return `${url.replace("/upload/", `/upload/w_${width}/`)} ${width}w`;
+    return `${url.replace('/upload/', `/upload/w_${width}/`)} ${width}w`;
+  },
+  getCloudinaryGallery: (url, width) => {
+    if (!url) return '';
+    // CHANGED: Upgraded from g_faces to g_auto:subject,c_fill
+    return url.replace('/upload/', `/upload/c_fill,g_auto:subject,w_${width}/`);
   },
 };
